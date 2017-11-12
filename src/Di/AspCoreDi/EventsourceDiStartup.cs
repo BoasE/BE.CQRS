2ce48046c31490c7086b8bc9a18e749129327a3b
BoasE <Boas@Enkler.OnMicrosoft.com>
2017-11-12 19:00:35 +0100

    improvements when using aspcore di

--- src/Di/AspCoreDi/EventsourceDiStartup.cs
+++ src/Di/AspCoreDi/EventsourceDiStartup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using BE.CQRS.Domain.Configuration;
+using BE.CQRS.Domain.DomainObjects;
 
 namespace BE.CQRS.Di.AspCore
 {
@@ -8,10 +9,20 @@ namespace BE.CQRS.Di.AspCore
         public static EventSourceConfiguration SetServiceCollectionActivator(this EventSourceConfiguration config, IServiceProvider serivces)
         {
             var activator = new ServiceCollectionActivator();
-            activator.UseProvider(serivces);
+
             config.Activator = activator;
 
             return config;
+        }
+
+        public static void UseServiceCollectionActivator(this IServiceProvider provider)
+        {
+            if (!(provider.GetService(typeof(IDomainObjectActivator)) is ServiceCollectionActivator activator))
+            {
+                throw new InvalidOperationException("UseServiceCollectionActivator requires a registered ServiceCollectionActivator");
+            }
+
+            activator.UseProvider(provider);
         }
     }
 }
