670ce7530d148799b6f9b08d6c94c1ab89b0a412
BoasE <Boas@Enkler.OnMicrosoft.com>
2017-11-12 19:15:44 +0100

    simplifying api for asp core di

--- src/Di/AspCoreDi/EventsourceDiStartup.cs
+++ src/Di/AspCoreDi/EventsourceDiStartup.cs
@@ -1,28 +1,29 @@
 ﻿using System;
 using BE.CQRS.Domain.Configuration;
 using BE.CQRS.Domain.DomainObjects;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BE.CQRS.Di.AspCore
 {
     public static class EventsourceDiStartup
     {
-        public static EventSourceConfiguration SetServiceCollectionActivator(this EventSourceConfiguration config, IServiceProvider serivces)
+        public static EventSourceConfiguration SetServiceProviderActivator(this EventSourceConfiguration config)
         {
             var activator = new ServiceCollectionActivator();
-
             config.Activator = activator;
 
             return config;
         }
 
-        public static void UseServiceCollectionActivator(this IServiceProvider provider)
+        public static void UseServiceProviderActivator(this IApplicationBuilder app)
         {
-            if (!(provider.GetService(typeof(IDomainObjectActivator)) is ServiceCollectionActivator activator))
+            if (!(app.ApplicationServices.GetRequiredService<IDomainObjectActivator>() is ServiceCollectionActivator activator))
             {
-                throw new InvalidOperationException("UseServiceCollectionActivator requires a registered ServiceCollectionActivator");
+                throw new InvalidOperationException("UseServiceCollectionActivator requires a registered IDomainObjectActivator Service with a type of \"ServiceCollectionActivator\"");
             }
 
-            activator.UseProvider(provider);
+            activator.UseProvider(app.ApplicationServices.GetRequiredService<IServiceProvider>());
         }
     }
 }
