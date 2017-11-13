cd83b402e3b71ae76753575e6f66f0c7cecfebdc
BoasE <Boas@Enkler.OnMicrosoft.com>
2017-11-13 23:30:54 +0100

    added missing file

---
+++ src/web/BE.CQRS.Web.AspCore/EventSourcedControllerBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using BE.CQRS.Domain;
+using BE.CQRS.Domain.Commands;
+using BE.FluentGuard;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE.CQRS.AspCore
+{
+    public abstract class EventSourcedControllerBase : ControllerBase
+    {
+        protected ICommandBus CommandBus { get; private set; }
+
+        protected IDomainObjectRepository DomainObjectRepository { get; private set; }
+
+        protected EventSourcedControllerBase(ICommandBus bus, IDomainObjectRepository domainObjectRepository)
+        {
+            Precondition.For(() => bus).NotNull();
+
+            CommandBus = bus;
+            DomainObjectRepository = domainObjectRepository;
+        }
+
+        protected async Task<ActionResult> AcceptCommand(ICommand command)
+        {
+            await CommandBus.EnqueueAsync(command);
+
+            return Accepted();
+        }
+
+        protected async Task<ActionResult> AcceptCommand(ICommand command, string uri)
+        {
+            await CommandBus.EnqueueAsync(command);
+
+            return Accepted(uri);
+        }
+
+        protected async Task<ActionResult> AcceptCommand(ICommand command, string uri, object value)
+        {
+            await CommandBus.EnqueueAsync(command);
+
+            return Accepted(uri, value);
+        }
+    }
+}
