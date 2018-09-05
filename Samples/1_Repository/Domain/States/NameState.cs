5cb075642c229631751e53b3a46ebe2c55536e76
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2018-09-05 11:34:55 +0200

    implement MongoDb Sample and remove deprecated EventStore sample

---
+++ Samples/1_Repository/Domain/States/NameState.cs
@@ -0,0 +1,15 @@
+﻿using BE.CQRS.Domain.States;
+using Domain.Events;
+
+namespace Domain.States
+{
+    public class NameState : StateBase
+    {
+        public string Name { get; set; }
+
+        public void On(CustomerCreatedFromConsoleEvent @event)
+        {
+            Name = @event.Name;
+        }
+    }
+}
--- Samples/1_Repository/RepositorySamples.MongoDb/Program.cs
+++ Samples/1_Repository/RepositorySamples.MongoDb/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Domain;
+using Domain.States;
+using MongoDB.Driver;
+using RepositorySamples.MongoDb;
+using System.Reactive.Linq;
+using Domain.Commands;
 
 namespace RepositorySamples
 {
     class Program
     {
-        static void Main(string[] args)
+        static Random Randomizer = new Random((int)DateTime.Now.TimeOfDay.TotalMilliseconds);
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting application...");
+            Console.WriteLine("Adding CQRS...");
+            string url = "mongodb://localhost:27017/";
+
+            Console.WriteLine(url);
+            IMongoDatabase mongodb = new MongoClient(url).GetDatabase("hellobecqrs");
+            var repo = CQRSBooter.AddCqrs(mongodb);
+            var id = Randomizer.Next();
+            var customer = new Customer(id.ToString());
+            customer.CreateNewCustomer(new CreateCustomerFromConsoleCommand()
+            {
+                Name = "Contoso"
+            });
+            await repo.SaveAsync(customer);
+            var persistedCustomer = await repo.Get<Customer>(id.ToString());
+            Console.WriteLine($"Persisted customer name: {persistedCustomer.State<NameState>().Name}");
         }
     }
 }
