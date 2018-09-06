2b3e8800b29e7839a2a5a3dba485080fe396ed4e
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2018-09-06 22:49:17 +0200

    Add Domain objects to Domain project

---
+++ Samples/2_ASPNET_Core/AspNetCoreSample.Domain/Commands/CreateCustomerFromConsoleCommand.cs
@@ -0,0 +1,12 @@
+﻿using BE.CQRS.Domain.Commands;
+
+namespace AspNetCoreSample.Domain.Commands
+{
+    public sealed class CreateCustomerFromConsoleCommand : ICommand // All Commands must be dervied from ICommand
+    {
+        public string DomainObjectId { get; set; }
+
+        public string Name { get; set; }
+        
+    }
+}
---
+++ Samples/2_ASPNET_Core/AspNetCoreSample.Domain/Events/CustomerCreatedFromConsoleEvent.cs
@@ -0,0 +1,12 @@
+﻿using BE.CQRS.Domain.Events;
+
+namespace AspNetCoreSample.Domain.Events
+{
+    public sealed class CustomerCreatedFromConsoleEvent : EventBase
+    {
+        public string Name { get; set; }
+        public CustomerCreatedFromConsoleEvent()
+        {
+        }
+    }
+}
