79a82e6be79283d96e2eac2b2e95ffa1fba8f7a7
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2018-09-06 23:19:13 +0200

    Add CQRSBooter / appsettings

--- Samples/2_ASPNET_Core/AspNetCoreSample/Startup.cs
+++ Samples/2_ASPNET_Core/AspNetCoreSample/Startup.cs
@@ -1,31 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreSample
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            Console.WriteLine("Configuring Services...");
+            services
+                .AddCors()
+                .AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            Console.WriteLine("Configuring Application...");
+
+            app.UseCqrs();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
         }
     }
 }
