d34eda4f483d4b56a3d2e602dc5cdc967e6d56c8
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2018-09-12 16:47:07 +0200

    implement GET customer by domain object id

--- Samples/2_ASPNET_Core/AspNetCoreSample/Controllers/CustomersController.cs
+++ Samples/2_ASPNET_Core/AspNetCoreSample/Controllers/CustomersController.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Reactive.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using AspNetCoreSample.Controllers.Models;
+using AspNetCoreSample.Domain;
 using AspNetCoreSample.Domain.Commands;
+using AspNetCoreSample.Domain.States;
+using BE.CQRS.Domain;
 using BE.CQRS.Domain.Commands;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,10 +16,12 @@ namespace AspNetCoreSample.Controllers
     public class CustomersController : Controller
     {
         private readonly ICommandBus bus;
+        private readonly IDomainObjectRepository repository;
 
-        public CustomersController(ICommandBus bus)
+        public CustomersController(ICommandBus bus, IDomainObjectRepository repository)
         {
             this.bus = bus;
+            this.repository = repository;
         }
 
         [HttpPost]
@@ -31,6 +38,19 @@ namespace AspNetCoreSample.Controllers
             acceptedResult.Location = new Uri($"/api/customers/{customerId}", UriKind.Relative).ToString();
 
             return acceptedResult;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] string id)
+        {
+            var customer = await repository.Get<Customer>(id);
+            var model = new ReadCustomerModel()
+            {
+                Name = customer.State<NameState>().Name
+            };
+
+            return new ObjectResult(model);
         }
     }
 }
