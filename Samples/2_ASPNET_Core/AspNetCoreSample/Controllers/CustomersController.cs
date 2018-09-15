5a991cd8ee0884e62bbf32ccc811501b8f0778b2
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2018-09-15 08:03:40 +0200

    Add get by Id method in sample 2 (#18)
    * return id of new customer domain object in accept results location header
    
    * set customer name from http content
    
    * refactor: move CreateCustomerModel to own file
    
    * implement GET customer by domain object id

--- Samples/2_ASPNET_Core/AspNetCoreSample/Controllers/CustomersController.cs
+++ Samples/2_ASPNET_Core/AspNetCoreSample/Controllers/CustomersController.cs
@@ -1,6 +1,12 @@
 using System;
+using System.Reactive.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using AspNetCoreSample.Controllers.Models;
+using AspNetCoreSample.Domain;
 using AspNetCoreSample.Domain.Commands;
+using AspNetCoreSample.Domain.States;
+using BE.CQRS.Domain;
 using BE.CQRS.Domain.Commands;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,27 +16,41 @@ namespace AspNetCoreSample.Controllers
     public class CustomersController : Controller
     {
         private readonly ICommandBus bus;
+        private readonly IDomainObjectRepository repository;
 
-        public CustomersController(ICommandBus bus)
+        public CustomersController(ICommandBus bus, IDomainObjectRepository repository)
         {
             this.bus = bus;
+            this.repository = repository;
         }
 
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] CreateCustomerModel model)
         {
-            
+            var customerId = Guid.NewGuid().ToString();
             await bus.EnqueueAsync(new CreateCustomerFromApiCommand()
             {
-                DomainObjectId = Guid.NewGuid().ToString(), //Mandatory Id
-                Name = "Contoso" // Should be applied from model
+                DomainObjectId = customerId, //Mandatory Id
+                Name = model.Name
             });
 
-            return new AcceptedResult();
-        }
-    }
+            var acceptedResult = new AcceptedResult();
+            acceptedResult.Location = new Uri($"/api/customers/{customerId}", UriKind.Relative).ToString();
 
-    public class CreateCustomerModel
-    {
+            return acceptedResult;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] string id)
+        {
+            var customer = await repository.Get<Customer>(id);
+            var model = new ReadCustomerModel()
+            {
+                Name = customer.State<NameState>().Name
+            };
+
+            return new ObjectResult(model);
+        }
     }
 }
---
+++ Samples/2_ASPNET_Core/AspNetCoreSample/Controllers/Models/CreateCustomerModel.cs
@@ -0,0 +1,7 @@
+namespace AspNetCoreSample.Controllers.Models
+{
+    public class CreateCustomerModel
+    {
+        public string Name { get; set; }
+    }
+}
