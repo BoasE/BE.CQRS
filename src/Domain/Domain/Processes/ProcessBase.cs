27bdf5be805589f793b838f4a93b0791f36cff40
BoasE <Boas@Enkler.OnMicrosoft.com>
2018-09-25 18:25:21 +0200

    minor logging improvements

--- src/Domain/Domain/Processes/ProcessBase.cs
+++ src/Domain/Domain/Processes/ProcessBase.cs
@@ -1,22 +1,30 @@
 ﻿using System.Threading.Tasks;
 using BE.CQRS.Domain.Commands;
+using Microsoft.Extensions.Logging;
 
 namespace BE.CQRS.Domain.Processes
 {
     public abstract class ProcessBase<TCommand> : IProcess<TCommand> where TCommand : ICommand
     {
+        private readonly ILogger logger;
+        private readonly string processName; 
+        
         protected ICommandBus CommandBus { get; }
 
         protected IDomainObjectRepository Repository { get; }
 
-        protected ProcessBase(ICommandBus commandBus, IDomainObjectRepository repository)
+        protected ProcessBase(ICommandBus commandBus, IDomainObjectRepository repository,ILoggerFactory factory)
         {
+            processName = this.GetType().Name;
+            logger = factory.CreateLogger<ProcessBase<TCommand>>();
             CommandBus = commandBus;
             Repository = repository;
         }
 
         public Task<ProcessResult> ExecuteAsync(TCommand command)
         {
+            
+            logger.LogTrace("Running Process {processName} is started",processName);
             return ExecuteInternalAsync(command);
         }
 
