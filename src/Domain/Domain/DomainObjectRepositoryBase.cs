dd1f61ac8f81cbbe5df148ad0c70895e33d523f2
Boas <Boas@Enkler.OnMicrosoft.com>
2019-04-02 22:49:31 +0200

    optimized code

--- src/Domain/Domain/DomainObjectRepositoryBase.cs
+++ src/Domain/Domain/DomainObjectRepositoryBase.cs
@@ -76,135 +76,114 @@ namespace BE.CQRS.Domain
             bool check = domainObject.CheckVersionOnSave && !preventVersionCheck;
             AppendResult result = await SaveUncomittedEventsAsync(domainObject, check);
 
-            if (!result.HadWrongVersion)
+            if (!result.HadWrongVersion && configuration.PostSavePipeline != null)
             {
-                if (configuration.PostSavePipeline != null)
+                foreach (var @event in domainObject.GetUncommittedEvents())
                 {
-                    foreach (var @event in domainObject.GetUncommittedEvents())
-                    {
-                        configuration.PostSavePipeline(@event);
-                    }
+                    configuration.PostSavePipeline(@event);
                 }
             }
-
-            domainObject.CommitChanges(result.CurrentVersion);
-            watch.Stop();
-
-
-            logger.LogTrace("Saved {count} events for \"{type}\" in {watch.ElapsedMilliseconds}ms", count, type,
-                watch.ElapsedMilliseconds);
-            return result;
         }
 
-        public Task<long> GetVersion<T>(string id) where T : class, IDomainObject
-        {
-            string streamName = ResolveStreamName(id, typeof(T));
+        domainObject.CommitChanges(result.CurrentVersion);
+        watch.Stop();
 
-            return GetVersion(streamName);
-        }
 
-        public Task<long> GetVersion(Type domainObjectType, string id)
-        {
-            string streamName = ResolveStreamName(id, domainObjectType);
-
-            return GetVersion(streamName);
-        }
-
-        protected abstract Task<long> GetVersion(string streamNaeme);
-
-        public Task<bool> Exists<T>(T domainobject) where T : class, IDomainObject
-        {
-            return Exists<T>(domainobject.Id);
-        }
-
-        public Task<bool> Exists<T>(string id) where T : class, IDomainObject
-        {
-            string stream = ResolveStreamName(id, typeof(T));
-
-            return ExistsStream(stream);
-        }
-
-        public IObservable<T> Get<T>(string id) where T : class, IDomainObject
-        {
-            return Get<T>(id, CancellationToken.None);
-        }
-
-        public IObservable<T> Get<T>(string id, ISet<Type> eventTypes, CancellationToken token)
-            where T : class, IDomainObject
-        {
-            Type type = typeof(T);
-
-            string streamName = ResolveStreamName(id, type);
-            logger.LogTrace("Reading events for type \"{type}\"-{id} ...", type, id);
-
-            return ReadEvents(streamName, eventTypes, token)
-                .ToArray()
-                .Select(events =>
-                    {
-                        var instance = configuration.Activator.Resolve<T>(id);
-
-                        instance.ApplyEvents(events, eventTypes);
-                        instance.ApplyConfig(configuration);
-                        return instance;
-                    }
-                );
-        }
-
-        public IObservable<T> Get<T>(string id, CancellationToken token) where T : class, IDomainObject
-        {
-            Type type = typeof(T);
-
-            string streamName = ResolveStreamName(id, type);
-            logger.LogTrace("Reading events for type \"{type}\"-{id} ...", type, id);
-
-            return ReadEvents(streamName, token)
-                .ToArray()
-                .Select(events =>
-                    {
-                        var instance = configuration.Activator.Resolve<T>(id);
-                        instance.ApplyEvents(events, null);
-                        instance.ApplyConfig(configuration);
-                        return instance;
-                    }
-                );
-        }
-
-        public IObservable<IDomainObject> Get(string id, Type domainObjectType)
-        {
-            return Get(id, domainObjectType, CancellationToken.None);
-        }
-
-        public IObservable<IDomainObject> Get(string id, Type domainObjectType, CancellationToken token)
-        {
-            string streamName = ResolveStreamName(id, domainObjectType);
-
-            return ReadEvents(streamName, token)
-                .ToArray()
-                .Select(events =>
-                    {
-                        IDomainObject instance = configuration.Activator.Resolve(domainObjectType, id);
-                        instance.ApplyEvents(events, null);
-                        instance.ApplyConfig(configuration);
-                        return instance;
-                    }
-                );
-        }
-
-        protected abstract Task<bool> ExistsStream(string streamName);
-
-        protected abstract string ResolveStreamName(string id, Type aggregateType);
-
-        protected abstract Task<AppendResult> SaveUncomittedEventsAsync<T>(T domainObject, bool versionCheck)
-            where T : class, IDomainObject;
-
-        protected abstract IObservable<IEvent> ReadEvents(string streamName, CancellationToken token);
-
-        protected abstract IObservable<IEvent> ReadEvents(string streamName, ISet<Type> eventTypes,
-            CancellationToken token);
-
-        public virtual IDomainObject New(Type domainObjectType, string id)
-        {
-            return configuration.Activator.Resolve(domainObjectType, id);
-        }
+        logger.LogTrace("Saved {count} events for \"{type}\" in {watch.ElapsedMilliseconds}ms", count, type,
+        watch.ElapsedMilliseconds);
+        return result;
     }
+
+    public Task<long> GetVersion<T>(string id) where T : class, IDomainObject
+    {
+    string streamName = ResolveStreamName(id, typeof(T));
+        return GetVersion(streamName);
+    }
+
+    public Task<long> GetVersion(Type domainObjectType, string id)
+    {
+    string streamName = ResolveStreamName(id, domainObjectType);
+        return GetVersion(streamName);
+    }
+
+    protected abstract Task<long> GetVersion(string streamNaeme);
+
+    public Task<bool> Exists<T>(T domainobject) where T : class, IDomainObject
+    {
+    return Exists<T>(domainobject.Id);
+}
+
+public Task<bool> Exists<T>(string id) where T : class, IDomainObject
+{
+string stream = ResolveStreamName(id, typeof(T));
+    return ExistsStream(stream);
+}
+
+public IObservable<T> Get<T>(string id) where T : class, IDomainObject
+{
+return Get<T>(id, CancellationToken.None);
+}
+public IObservable<T> Get<T>(string id, ISet<Type> eventTypes, CancellationToken token)
+where T : class, IDomainObject
+{
+Type type = typeof(T);
+string streamName = ResolveStreamName(id, type);
+logger.LogTrace("Reading events for type \"{type}\"-{id} ...", type, id);
+return ReadEvents(streamName, eventTypes, token)
+.ToArray()
+.Select(events =>
+{
+var instance = configuration.Activator.Resolve<T>(id);
+instance.ApplyEvents(events, eventTypes);
+instance.ApplyConfig(configuration);
+return instance;
+}
+);
+}
+public IObservable<T> Get<T>(string id, CancellationToken token) where T : class, IDomainObject
+{
+Type type = typeof(T);
+string streamName = ResolveStreamName(id, type);
+logger.LogTrace("Reading events for type \"{type}\"-{id} ...", type, id);
+return ReadEvents(streamName, token)
+.ToArray()
+.Select(events =>
+{
+var instance = configuration.Activator.Resolve<T>(id);
+instance.ApplyEvents(events, null);
+instance.ApplyConfig(configuration);
+return instance;
+}
+);
+}
+public IObservable<IDomainObject> Get(string id, Type domainObjectType)
+{
+return Get(id, domainObjectType, CancellationToken.None);
+}
+public IObservable<IDomainObject> Get(string id, Type domainObjectType, CancellationToken token)
+{
+string streamName = ResolveStreamName(id, domainObjectType);
+return ReadEvents(streamName, token)
+.ToArray()
+.Select(events =>
+{
+IDomainObject instance = configuration.Activator.Resolve(domainObjectType, id);
+instance.ApplyEvents(events, null);
+instance.ApplyConfig(configuration);
+return instance;
+}
+);
+}
+protected abstract Task<bool> ExistsStream(string streamName);
+protected abstract string ResolveStreamName(string id, Type aggregateType);
+protected abstract Task<AppendResult> SaveUncomittedEventsAsync<T>(T domainObject, bool versionCheck)
+where T : class, IDomainObject;
+protected abstract IObservable<IEvent> ReadEvents(string streamName, CancellationToken token);
+protected abstract IObservable<IEvent> ReadEvents(string streamName, ISet<Type> eventTypes,
+CancellationToken token);
+public virtual IDomainObject New(Type domainObjectType, string id)
+{
+return configuration.Activator.Resolve(domainObjectType, id);
+}
+}
 }
