6adff96c541e258880913468862978a7bf509fae
Boas <Boas@Enkler.OnMicrosoft.com>
2019-10-04 23:31:31 +0200

    fixes

--- src/TestFramework/Testrunner/Program.cs
+++ src/TestFramework/Testrunner/Program.cs
@@ -116,8 +116,8 @@ namespace Testrunner
 
             IMongoDatabase db =
                 new MongoClient(
-                        "mongodb://localhost:27017/?readPreference=primary")
-                    .GetDatabase("eventTests");
+                        "mongodb+srv://soccercise_api:5ZBtChpPcvVEK0FN@cluster0-fyly5.gcp.mongodb.net/test?retryWrites=true")
+                    .GetDatabase("eventTests2");
 
             services.AddSingleton<IMongoDatabase>(db);
         }
