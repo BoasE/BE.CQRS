3a4d0793af1c2d9b6172d50e377850003196206f
BoasE <Boas.Enkler@Busliniensuche.de>
2020-02-03 13:30:01 +0100

    Removed obsolete indexes

--- src/Data/BE.CQRS.Data.MongoDb/Commits/IndexDefinitions.cs
+++ src/Data/BE.CQRS.Data.MongoDb/Commits/IndexDefinitions.cs
@@ -14,36 +14,13 @@ namespace BE.CQRS.Data.MongoDb.Commits
                     new CreateIndexOptions { Unique = true, Name = "cqrs_ord" }),
 
                 new CreateIndexModel<EventCommit>(Builders<EventCommit>.IndexKeys
-                        .Descending(x => x.AggregateId),
-                    new CreateIndexOptions() { Name = "cqrs_aid" }),
-
-                new CreateIndexModel<EventCommit>(Builders<EventCommit>.IndexKeys
-                        .Descending(x => x.AggregateType),
-                    new CreateIndexOptions() { Name = "cqrs_aty" }),
-
-                new CreateIndexModel<EventCommit>(Builders<EventCommit>.IndexKeys
                         .Descending(x => x.AggregateId).Descending(x => x.AggregateType),
                     new CreateIndexOptions() { Name = "cqrs_aid_aty" }),
 
                 new CreateIndexModel<EventCommit>(Builders<EventCommit>.IndexKeys
                         .Descending(x => x.AggregateId).Descending(x => x.AggregateType)
-                        .Descending(x => x.VersionEvents),
-                    new CreateIndexOptions { Unique = true, Name = "cqrs_aid_aty_ve" }),
-
-                new CreateIndexModel<EventCommit>(Builders<EventCommit>.IndexKeys
-                        .Descending(x => x.AggregateId).Descending(x => x.AggregateType)
-                        .Descending(x => x.VersionCommit),
-                    new CreateIndexOptions { Unique = true, Name = "cqrs_aid_aty_vc" }),
-
-                new CreateIndexModel<EventCommit>(Builders<EventCommit>.IndexKeys
-                        .Descending(x => x.AggregateId).Descending(x => x.AggregateType)
                         .Ascending(x => x.AllEventTypes),
                     new CreateIndexOptions() { Name = "cqrs_aid,aty_aet" }),
-
-                new CreateIndexModel<EventCommit>(Builders<EventCommit>.IndexKeys
-                        .Descending(x => x.AggregateId).Descending(x => x.AggregateType)
-                        .Ascending(x => x.AllEventTypes).Descending(x => x.Ordinal),
-                    new CreateIndexOptions { Unique = true, Name = "cqrs_aid_aty_aet_ord" }),
 
                 new CreateIndexModel<EventCommit>(Builders<EventCommit>.IndexKeys
                         .Descending(x => x.AggregateId).Descending(x => x.AggregateType)
