e5adc8ddae81c638cf2799991dcdba12318e2317
Boas <Boas@Enkler.OnMicrosoft.com>
2020-03-29 18:54:11 +0200

    Fixes for unity Container

--- src/Di/UnityDi/EventsourceDiStartup.cs
+++ src/Di/UnityDi/EventsourceDiStartup.cs
@@ -1,16 +1,27 @@
-﻿using BE.CQRS.Di.Unity;
+﻿using System;
+using BE.CQRS.Di.Unity;
+using BE.CQRS.Domain;
 using BE.CQRS.Domain.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Practices.Unity;
 
 namespace BE.CQRS.Di.AspCore
 {
     public static class EventsourceDiStartup
     {
-        public static EventSourceConfiguration SetServiceCollectionActivator(this EventSourceConfiguration config, IUnityContainer container)
+        public static IServiceCollection AddUnityActivator(this IServiceCollection services, IUnityContainer container)
         {
-            config.Activator = new UnityDomainObjectActivator(container);
+            services
+                .TryAddSingleton<EventsourceDIContext>(x =>
+                {
+                    var activator = new UnityDomainObjectActivator(container);
+                    return new EventsourceDIContext(activator, activator);
+                    
+                });
 
-            return config;
+
+            return services;
         }
     }
 }
--- src/Di/UnityDi/UnityDomainObjectActivator.cs
+++ src/Di/UnityDi/UnityDomainObjectActivator.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Reflection;
 using BE.CQRS.Domain.DomainObjects;
+using BE.CQRS.Domain.States;
 using Microsoft.Practices.Unity;
 
 namespace BE.CQRS.Di.Unity
 {
-    public sealed class UnityDomainObjectActivator : IDomainObjectActivator
+    public sealed class UnityDomainObjectActivator : IDomainObjectActivator,IStateActivator
     {
         private static readonly TypeInfo DomainObjectInfo = typeof(IDomainObject).GetTypeInfo();
         private readonly IUnityContainer container;
@@ -23,6 +24,16 @@ namespace BE.CQRS.Di.Unity
         public IDomainObject Resolve(Type domainObjectType, string id)
         {
             return (IDomainObject)container.Resolve(domainObjectType, new OrderedParametersOverride(id));
+        }
+
+        public T ResolveState<T>() where T : class, IState
+        {
+            return container.Resolve<T>();
+        }
+
+        public IState ResolveState(Type denormalizerType)
+        {
+            return (IState) container.Resolve(denormalizerType);
         }
     }
 }
